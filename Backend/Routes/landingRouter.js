dd74289faa4891a53e0258d8d3165c2b4580e9cd
SupunBHerath <supunbherath@gmail.com>
2024-09-04 01:01:12 +0530

    .

--- Backend/Routes/landingRouter.js
+++ Backend/Routes/landingRouter.js
@@ -12,7 +12,7 @@ import multer from 'multer';
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10MB
+  limits: { fileSize: 20 * 1024 * 1024 }, // Limit file size to 10MB
 }).fields([{ name: 'image', maxCount: 1 }]);
 
 
